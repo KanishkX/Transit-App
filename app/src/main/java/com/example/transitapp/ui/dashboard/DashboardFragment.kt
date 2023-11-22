3a938c01bf03e5e30c61548d6d0f3e7e384cff1e
W0478410 <Kanishkmain@gmail.com>
2023-11-22 18:40:41 -0400

    Sprint #3 autocomplete

--- app/src/main/java/com/example/transitapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/transitapp/ui/dashboard/DashboardFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.transitapp.R
 import com.example.transitapp.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -16,6 +19,7 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,10 +32,19 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textDashboard
+//        dashboardViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        val busNumbers = resources.getStringArray(R.array.bus)
+
+        val autoCompleteTextView: AutoCompleteTextView = root.findViewById(R.id.autoComplete)
+
+//        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, busNumbers)
+//        autoCompleteTextView.setAdapter(adapter)
+        // Create the adapter and set it to the AutoCompleteTextView
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, busNumbers)
+        autoCompleteTextView.setAdapter(adapter)
         return root
     }
 
